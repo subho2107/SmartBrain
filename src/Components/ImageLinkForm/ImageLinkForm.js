5bc0eaa906b97510bc476200519e44cc5fc18039
Tirthankar Bhattacharya <shubho2001g@gmail.com>
2020-12-24 12:31:24 +0530

    update

---
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ImageLinkForm.css';
+const ImageLinkForm = ({onInputChange, onButtonSubmit})=>{
+	return(
+		<div>
+			<p className = 'f3'>
+				{'this Magic brain will detect faces in your picture. Give it a go!'}
+			</p>
+			<div className = 'center'>
+				<div className = 'form center pa4 br3 shadow-5'>
+					<input className = 'f4 pa2 w-70 center' type='tex'onChange = {onInputChange}/>
+					<button onClick = {onButtonSubmit} className = 'w-30 grow f4 link ph3 pv2 div white bg-light-purple'>Detect</button>
+				</div>
+			</div>
+		</div>
+		);
+}
+export default ImageLinkForm;
